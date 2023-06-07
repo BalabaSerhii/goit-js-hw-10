b7c9eab21d4bbe1d9f626dccd440d4dbf84802a5
BalabaSerhii <balabaserhii@gmail.com>
2023-06-07 17:13:45 +0200

    d

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,27 +1,24 @@
 import { renderBreedsSelect } from "../index"
-const APIKEY = 'live_yLDkUr3dbr9zSfZaJmQrZc8jkq2mgsHyEyNMPqfIALTiwT0FN4h0RODPgtWAcrzr'
+const APIKEY = 'live_A0BddCBCvw3bx0QQ33fvXeI93mVx6l59VBUsyj7uHjfZvkQnUvjiGTaolms8FVAg'
 
 function fetchBreeds() {
-    return fetch(
-        `https://api.thecatapi.com/v1/breeds?${APIKEY}`
-        ).then((response) => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        });
+  return fetch(
+    'https://api.thecatapi.com/v1/breeds?${APIKEY}'
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
     }
-
+    return response.json();
+  });
+}
 
 function fetchCatByBreed(breedId) {
-  return fetch(`https://api.thecatapi.com/v1/images/${breedId}?api_key=${APIKEY}&breed_ids=${breedId}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
+  return fetch('https://api.thecatapi.com/v1/images/${breedId}?api_key=${APIKEY}&breed_ids=${breedId}').then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json()
+  });
 }
- 
-export { fetchBreeds, fetchCatByBreed }
 
+export { fetchBreeds, fetchCatByBreed }
